1dba47d1742bd06c5ea262304da614e4b3448332
chris <t315113@hotmail.com>
2025-05-28 10:50:20 +0800

    maven打包錯誤，故將介面宣告ClinicDao改成ClinicDAO

--- ires-system/src/main/java/web/clinic/dao/ClinicDAO.java
+++ ires-system/src/main/java/web/clinic/dao/ClinicDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 import core.dao.CoreDao;
 import web.clinic.entity.Clinic;
 
-public interface ClinicDao extends CoreDao<Clinic, String> {
+public interface ClinicDAO extends CoreDao<Clinic, String> {
 
 	List<Clinic> UpdatePsd(int clinicId);
 
