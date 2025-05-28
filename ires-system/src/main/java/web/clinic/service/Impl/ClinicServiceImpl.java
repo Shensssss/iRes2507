c1443e159ca7d3cdf046cb18c95ed446c40635a4
chris <t315113@hotmail.com>
2025-05-28 10:55:59 +0800

    ClinicDao改成ClinicDAO

--- ires-system/src/main/java/web/clinic/service/Impl/ClinicServiceImpl.java
+++ ires-system/src/main/java/web/clinic/service/Impl/ClinicServiceImpl.java
@@ -5,7 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import web.clinic.dao.ClinicDao;
+import web.clinic.dao.ClinicDAO;
 import web.clinic.entity.Clinic;
 import web.clinic.service.ClinicService;
 
@@ -13,11 +13,11 @@ import web.clinic.service.ClinicService;
 @Transactional
 public class ClinicServiceImpl implements ClinicService{
 	@Autowired
-	private ClinicDao clinicDao;
+	private ClinicDAO clinicDAO;
 	
 	@Override
 	public void editPsd(Clinic clinic) {
-		clinicDao.update(clinic);
+		clinicDAO.update(clinic);
 		
 	}
 
