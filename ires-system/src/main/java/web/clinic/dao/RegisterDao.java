dc659e874ca00bd284d159f542cf1c3b818e25e7
Brant <brantlin007@gmail.com>
2025-06-11 23:16:51 +0800

    註冊登入功能語法更新

--- ires-system/src/main/java/web/clinic/dao/RegisterDao.java
+++ ires-system/src/main/java/web/clinic/dao/RegisterDao.java
@@ -9,4 +9,7 @@ public interface RegisterDao extends CoreDao<Clinic, String>{
 	
 	Clinic selectForLogin(String account, String password);
 	
+	Clinic selectForPassword(String agencyId, String account);
+	
+	int updatePassword(String account, String password);
 }
