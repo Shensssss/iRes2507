744089ce7750cb5fbb4147f2543ff6464c5a928d
Billy <wl01571990@gmail.com>
2025-06-16 22:12:56 +0800

    更新前端

--- ires-system/src/main/webapp/Patient/static/js/notification.js
+++ ires-system/src/main/webapp/Patient/static/js/notification.js
@@ -1,12 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const notifications = document.querySelectorAll(".notification");
-
-    notifications.forEach((notif, index) => {
-        setTimeout(() => {
-            notif.style.animation = `fadeIn 0.5s ease-in-out ${index * 0.3}s forwards`;
-        }, index * 200);
-    });
-});
+// 刪除通知的函數
 function deleteNotification(button) {
-    button.parentElement.style.display = 'none';
+    const notification = button.parentElement;
+    notification.remove(); // 直接刪除通知元素
 }
