356255a0090a1ec310b887f8cba81f48635acf47
chris <t315113@hotmail.com>
2025-06-23 09:24:51 +0800

    加入SpringMVC檔案上傳組態設定

--- ires-system/src/main/java/core/config/SpringMvcConfig.java
+++ ires-system/src/main/java/core/config/SpringMvcConfig.java
@@ -10,11 +10,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({ "web.*.controller" })
 public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
